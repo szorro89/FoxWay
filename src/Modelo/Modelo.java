9ec686f2eedbad8a513a7fe237a31e6dc242089b
LightFox <szorro89@gmail.com>
2015-05-21 09:46:38 -0500

    Avance lectura de velocidades hecho
    Se leen las velocidades y quedan almacenadas en el sistema como un DTO

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -3,6 +3,9 @@ package Modelo;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
@@ -10,6 +13,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -17,9 +24,14 @@ import javax.swing.JPanel;
  */
 public class Modelo{
     
+    static final String PREFIJO_RUTA = "src\\zData";
+    static final String WIN_APPENDER = "\\";
+    
+    
     public static void cargarDatos(String tipoDato, String mes, String anio){
-        if(tipoDato != "" && mes != "Mes" && anio != "Año"){
-        System.out.println(tipoDato + " " + mes + " " + anio);
+        if(!"".equals(tipoDato) && !"Mes".equals(mes) && !"Año".equals(anio)){
+            System.out.println(tipoDato + " " + mes + " " + anio);
+            List<VelocidadDTO> velocidades = leerVelocidades(tipoDato,mes,anio);
         }else
             JOptionPane.showMessageDialog(new JFrame(), "Por favor ingrese los datos solicitados");
     }
@@ -140,4 +152,260 @@ public class Modelo{
         infoApp.add(content);
     }
     
+    static List<VelocidadDTO> leerVelocidades(String tipoDato, String mes, String anio){
+        List<VelocidadDTO> velocidadesDTO = new ArrayList<>();
+        try{
+            System.out.println("leerVelocidades");
+            String ruta = hacerRuta(tipoDato, mes, anio);
+            
+            File folder = new File(ruta);
+            File[] listOfFiles = folder.listFiles();
+            
+            for(File file: listOfFiles){
+                if(!file.getName().startsWith("~")){
+                    System.out.println(ruta + WIN_APPENDER + file.getName() + "--------------------------------------------------------------");
+                    VelocidadDTO vel = new VelocidadDTO();
+                    FileInputStream stream = new FileInputStream(new File(ruta + WIN_APPENDER + file.getName()));
+                    XSSFWorkbook workbook = new XSSFWorkbook(stream);
+                    XSSFSheet worksheet = workbook.getSheet("1.IDENTIFICACION");
+
+                    //Lectura nombreArchivo
+                    Row row1 = worksheet.getRow(9);
+                    Cell cell1 = row1.getCell(21);
+                    vel.setNombreArchivo(cell1.getStringCellValue());
+                    System.out.println("Nombre del archivo: " + vel.getNombreArchivo());
+                    
+                    //Lectura nombreCorredor
+                    Row row2 = worksheet.getRow(11);
+                    Cell cell2 = row2.getCell(21);
+                    vel.setNombreCorredor(cell2.getStringCellValue());
+                    System.out.println("Corredor vial: " + vel.getNombreCorredor());
+                    
+                    //Lectura fechaEstudio
+                    Row row3 = worksheet.getRow(25);
+                    Cell cell3 = row3.getCell(17);
+                    vel.setFechaEstudio(cell3.getStringCellValue());
+                    System.out.println("Fecha del estudio: " + vel.getFechaEstudio());
+                    
+                    //Lectura analisisPuntual
+                    Row row4 = worksheet.getRow(57);
+                    Cell cell4 = row4.getCell(16);
+                    vel.setAnalisisPuntual(cell4.getStringCellValue());
+                    System.out.println("El análisis: " + vel.getAnalisisPuntual());
+                    
+                    //Lectura descripcionTramos
+                    List<List<String>> tramos = new ArrayList<>();
+                    for(int i = 33 ; i < 55 ; i++){
+                        List<String> descripcion = new ArrayList<>();
+                        Row tramo = worksheet.getRow(i);
+                        int cont = 0;
+                        for(int j = 17 ; j < 20 ; j++){
+                            Cell desc = tramo.getCell(j);
+                            if(cont == 0 || cont ==1){
+                                if(!desc.getStringCellValue().isEmpty() || !desc.getStringCellValue().equals(""))
+                                    descripcion.add(desc.getStringCellValue());
+                            }else{
+                                if(desc.getNumericCellValue() != 0)
+                                    descripcion.add(String.valueOf(desc.getNumericCellValue()));
+                            }
+                            cont++;
+                        }
+                        tramos.add(descripcion);
+                    }
+                    vel.setDescripcionTramos(tramos);
+                    
+                    for(int i = 0; i<vel.getDescripcionTramos().size(); i++){
+                        for(int j = 0; j<vel.getDescripcionTramos().get(i).size(); j++){
+                            System.out.println("Tramos: " + vel.getDescripcionTramos().get(i).get(j));
+                        }
+                    }
+                    
+                    //Lectura tipoVehiculos
+                    List<String> tipoVehiculos = new ArrayList<>();
+                    for(int i = 31 ; i < 33 ; i++){
+                        Row tipo = worksheet.getRow(i);
+                        for(int j = 25 ; j < 29 ; j+=2){
+                            Cell vehiculo = tipo.getCell(j);
+                            if(!vehiculo.getStringCellValue().isEmpty() || !vehiculo.getStringCellValue().equals(""))
+                                tipoVehiculos.add(vehiculo.getStringCellValue());
+                        }
+                    }
+                    vel.setTipoVehiculo(tipoVehiculos);
+                    
+                    for(int i = 0; i<vel.getTipoVehiculo().size(); i++){
+                        System.out.println("Tipo vehiculo: " + vel.getTipoVehiculo().get(i));
+                    }
+                    
+                    //Lectura horarios de periodos
+                    List<List<String>> horarios = new ArrayList<>();
+                    for(int i = 34 ; i < 37 ; i++){
+                        List<String> periodos = new ArrayList<>();
+                        Row horario = worksheet.getRow(i);
+                        for(int j = 25 ; j < 29 ; j+=2){
+                            Cell periodo = horario.getCell(j);
+                            if(!periodo.getStringCellValue().isEmpty() || !periodo.getStringCellValue().equals(""))
+                                periodos.add(periodo.getStringCellValue());
+                        }
+                        horarios.add(periodos);
+                    }
+                    vel.setHorarioDePeriodos(horarios);
+                    
+                    for(int i = 0; i<vel.getHorarioDePeriodos().size(); i++){
+                        for(int j = 0; j<vel.getHorarioDePeriodos().get(i).size(); j++){
+                            System.out.println("Horarios: " + vel.getHorarioDePeriodos().get(i).get(j));
+                        }
+                    }
+                    
+                    //Lectura numEquipos
+                    List<List<String>> equipos = new ArrayList<>();
+                    for(int i = 37 ; i < 40 ; i++){
+                        List<String> num = new ArrayList<>();
+                        Row equipo = worksheet.getRow(i);
+                        int cont = 0;
+                        for(int j = 25 ; j < 29 ; j+=2){
+                            Cell numero = equipo.getCell(j);
+                            if(cont == 0){
+                                if(!numero.getStringCellValue().isEmpty() || !numero.getStringCellValue().equals(""))
+                                    num.add(numero.getStringCellValue());
+                            }
+                            else{
+                                if(numero.getNumericCellValue() != 0)
+                                    num.add(String.valueOf((int)numero.getNumericCellValue()));
+                            }
+                            cont++;
+                        }
+                        equipos.add(num);
+                    }
+                    vel.setNumEquipos(equipos);
+                    
+                    for(int i = 0; i<vel.getNumEquipos().size(); i++){
+                        for(int j = 0; j<vel.getNumEquipos().get(i).size(); j++){
+                            System.out.println("Equipos: " + vel.getNumEquipos().get(i).get(j));
+                        }
+                    }
+                    
+                    //Lectura de velocidades
+                    XSSFSheet worksheet1 = workbook.getSheet("4c.PERFIL DE VELOCIDAD");
+                    List<List<String>> velocidades = new ArrayList<>();
+                    int k = 15;
+                    while(worksheet1.getRow(k) != null){
+                        int cont = 0;
+                        Row tramo = worksheet1.getRow(k);
+                        List<String> velocidad = new ArrayList<>();
+                        for(int j = 1; j < 6; j++){
+                            Cell cell = tramo.getCell(j);
+                            if(cont == 0 || cont == 1){
+                                if(!cell.getStringCellValue().isEmpty() || !cell.getStringCellValue().equals(""))
+                                    velocidad.add(cell.getStringCellValue());
+                            }
+                            if(cont == 2 || cont == 3 || cont == 4){
+                                if(cell.getNumericCellValue() != 0)
+                                    velocidad.add(String.valueOf(cell.getNumericCellValue()));
+                            }
+                            cont++;
+                        }
+                        velocidades.add(velocidad);
+                        k++;
+                    }
+                    vel.setVelocidadesPromedio(velocidades);
+                    
+                    for(int i = 0; i<vel.getVelocidadesPromedio().size(); i++){
+                        for(int j = 0; j<vel.getVelocidadesPromedio().get(i).size(); j++){
+                            System.out.println("Velocidades: " + vel.getVelocidadesPromedio().get(i).get(j));
+                        }
+                    }
+                    
+                    //Lectura de datos definitivos
+                    XSSFSheet worksheet2 = workbook.getSheet("5b.RESUMEN MENSUAL");
+                    Row res1 = worksheet2.getRow(7);
+                    
+                    Cell ce1 = res1.getCell(8);
+                    vel.setVelTP_AM_NS_WE(ce1.getNumericCellValue());
+                    
+                    Cell ce2 = res1.getCell(9);
+                    vel.setVelTP_M_NS_WE(ce2.getNumericCellValue());
+                    
+                    Cell ce3 = res1.getCell(10);
+                    vel.setVelTP_PM_NS_WE(ce3.getNumericCellValue());
+                    
+                    Cell ce4 = res1.getCell(11);
+                    vel.setVelTPC_AM_NS_WE(ce4.getNumericCellValue());
+                    
+                    Cell ce5 = res1.getCell(12);
+                    vel.setVelTPC_M_NS_WE(ce5.getNumericCellValue());
+                    
+                    Cell ce6 = res1.getCell(13);
+                    vel.setVelTPC_PM_NS_WE(ce6.getNumericCellValue());
+                    
+                    Cell ce7 = res1.getCell(14);
+                    vel.setVelTPI_AM_NS_WE(ce7.getNumericCellValue());
+                    
+                    Cell ce8 = res1.getCell(15);
+                    vel.setVelTPI_M_NS_WE(ce8.getNumericCellValue());
+                    
+                    Cell ce9 = res1.getCell(16);
+                    vel.setVelTPI_PM_NS_WE(ce9.getNumericCellValue());
+                    
+                    Cell ce10 = res1.getCell(20);
+                    vel.setVelTP_AM_SN_EW(ce10.getNumericCellValue());
+                    
+                    Cell ce11 = res1.getCell(21);
+                    vel.setVelTP_M_SN_EW(ce11.getNumericCellValue());
+                    
+                    Cell ce12 = res1.getCell(22);
+                    vel.setVelTP_PM_SN_EW(ce12.getNumericCellValue());
+                    
+                    Cell ce13 = res1.getCell(23);
+                    vel.setVelTPC_AM_SN_EW(ce13.getNumericCellValue());
+                    
+                    Cell ce14 = res1.getCell(24);
+                    vel.setVelTPC_M_SN_EW(ce14.getNumericCellValue());
+                    
+                    Cell ce15 = res1.getCell(25);
+                    vel.setVelTPC_PM_SN_EW(ce15.getNumericCellValue());
+                    
+                    Cell ce16 = res1.getCell(26);
+                    vel.setVelTPI_AM_SN_EW(ce16.getNumericCellValue());
+                    
+                    Cell ce17 = res1.getCell(27);
+                    vel.setVelTPI_M_SN_EW(ce17.getNumericCellValue());
+                    
+                    Cell ce18 = res1.getCell(28);
+                    vel.setVelTPI_PM_SN_EW(ce18.getNumericCellValue());
+                    System.out.println("velTPI: " + vel.getVelTPI_PM_SN_EW());
+                    
+                    velocidadesDTO.add(vel);
+                }
+            }
+        }catch(Exception e){
+            System.out.println("Se ha generado un problema leyendo los archivos -> " + e );
+            e.printStackTrace();
+        }
+        System.out.println("Done!");
+        return velocidadesDTO;
+    }
+    
+    static String hacerRuta (String tipoDato, String mes, String anio){
+        String ruta = PREFIJO_RUTA + WIN_APPENDER + tipoDato + WIN_APPENDER + 
+                anio + WIN_APPENDER + mes;
+        return ruta;
+    }
+    
 }
+
+//                    FileInputStream stream = new FileInputStream(new File(ruta + WIN_APPENDER + file.getName()));
+//                    XSSFWorkbook workbook = new XSSFWorkbook(stream);
+//                    XSSFSheet worksheet = workbook.getSheet("1.IDENTIFICACION");
+//                    Iterator<Row> rowIterator = worksheet.iterator();
+//                    int i = 0;
+//                    while (rowIterator.hasNext()) {
+//                      Row row = rowIterator.next();
+//                      Iterator <Cell> cellIterator = row.cellIterator();
+//                      int j = 0;
+//                      while (cellIterator.hasNext()) {
+//                        Cell cell = cellIterator.next();
+//                        System.out.print(i + "," + j + ": "+ cell.getStringCellValue() + "\n");
+//                        j++;
+//                      }
+//                      i++;
+//                    }
--- src/Modelo/VelocidadDTO.java
+++ src/Modelo/VelocidadDTO.java
@@ -16,15 +16,66 @@ public class VelocidadDTO {
     private double lat;
     private double lon;
     private String nombreArchivo;
-    private String fechaMuestra;
-    private List<String> listaDato;
+    private String nombreCorredor;
+    private String fechaEstudio;
+    private String analisisPuntual;
+    private List<List<String>> descripcionTramos;
+    private List<String> tipoVehiculo;
+    private List<List<String>> horarioDePeriodos;
+    private List<List<String>> numEquipos;
+    private double velTP_AM_NS_WE;
+    private double velTP_M_NS_WE;
+    private double velTP_PM_NS_WE;
+    private double velTPC_AM_NS_WE;
+    private double velTPC_M_NS_WE;
+    private double velTPC_PM_NS_WE;
+    private double velTPI_AM_NS_WE;
+    private double velTPI_M_NS_WE;
+    private double velTPI_PM_NS_WE;
+    private double velTP_AM_SN_EW;
+    private double velTP_M_SN_EW;
+    private double velTP_PM_SN_EW;
+    private double velTPC_AM_SN_EW;
+    private double velTPC_M_SN_EW;
+    private double velTPC_PM_SN_EW;
+    private double velTPI_AM_SN_EW;
+    private double velTPI_M_SN_EW;
+    private double velTPI_PM_SN_EW;
+    private List<List<String>> velocidadesPromedio;
 
-    public VelocidadDTO(double lat, double lon, String nombreArchivo, String fechaMuestra, List<String> listaDato) {
+    public VelocidadDTO() {
+    }
+
+    public VelocidadDTO(double lat, double lon, String nombreArchivo, String nombreCorredor, String fechaEstudio, String analisisPuntual, List<List<String>> descripcionTramos, List<String> tipoVehiculo, List<List<String>> horarioDePeriodos, List<List<String>> numEquipos, double velTP_AM_NS_WE, double velTP_M_NS_WE, double velTP_PM_NS_WE, double velTPC_AM_NS_WE, double velTPC_M_NS_WE, double velTPC_PM_NS_WE, double velTPI_AM_NS_WE, double velTPI_M_NS_WE, double velTPI_PM_NS_WE, double velTP_AM_SN_EW, double velTP_M_SN_EW, double velTP_PM_SN_EW, double velTPC_AM_SN_EW, double velTPC_M_SN_EW, double velTPC_PM_SN_EW, double velTPI_AM_SN_EW, double velTPI_M_SN_EW, double velTPI_PM_SN_EW, List<List<String>> velocidadesPromedio) {
         this.lat = lat;
         this.lon = lon;
         this.nombreArchivo = nombreArchivo;
-        this.fechaMuestra = fechaMuestra;
-        this.listaDato = listaDato;
+        this.nombreCorredor = nombreCorredor;
+        this.fechaEstudio = fechaEstudio;
+        this.analisisPuntual = analisisPuntual;
+        this.descripcionTramos = descripcionTramos;
+        this.tipoVehiculo = tipoVehiculo;
+        this.horarioDePeriodos = horarioDePeriodos;
+        this.numEquipos = numEquipos;
+        this.velTP_AM_NS_WE = velTP_AM_NS_WE;
+        this.velTP_M_NS_WE = velTP_M_NS_WE;
+        this.velTP_PM_NS_WE = velTP_PM_NS_WE;
+        this.velTPC_AM_NS_WE = velTPC_AM_NS_WE;
+        this.velTPC_M_NS_WE = velTPC_M_NS_WE;
+        this.velTPC_PM_NS_WE = velTPC_PM_NS_WE;
+        this.velTPI_AM_NS_WE = velTPI_AM_NS_WE;
+        this.velTPI_M_NS_WE = velTPI_M_NS_WE;
+        this.velTPI_PM_NS_WE = velTPI_PM_NS_WE;
+        this.velTP_AM_SN_EW = velTP_AM_SN_EW;
+        this.velTP_M_SN_EW = velTP_M_SN_EW;
+        this.velTP_PM_SN_EW = velTP_PM_SN_EW;
+        this.velTPC_AM_SN_EW = velTPC_AM_SN_EW;
+        this.velTPC_M_SN_EW = velTPC_M_SN_EW;
+        this.velTPC_PM_SN_EW = velTPC_PM_SN_EW;
+        this.velTPI_AM_SN_EW = velTPI_AM_SN_EW;
+        this.velTPI_M_SN_EW = velTPI_M_SN_EW;
+        this.velTPI_PM_SN_EW = velTPI_PM_SN_EW;
+        this.velocidadesPromedio = velocidadesPromedio;
     }
 
     public double getLat() {
@@ -51,20 +102,212 @@ public class VelocidadDTO {
         this.nombreArchivo = nombreArchivo;
     }
 
-    public String getFechaMuestra() {
-        return fechaMuestra;
+    public String getNombreCorredor() {
+        return nombreCorredor;
     }
 
-    public void setFechaMuestra(String fechaMuestra) {
-        this.fechaMuestra = fechaMuestra;
+    public void setNombreCorredor(String nombreCorredor) {
+        this.nombreCorredor = nombreCorredor;
     }
 
-    public List<String> getListaDato() {
-        return listaDato;
+    public String getFechaEstudio() {
+        return fechaEstudio;
     }
 
-    public void setListaDato(List<String> listaDato) {
-        this.listaDato = listaDato;
+    public void setFechaEstudio(String fechaEstudio) {
+        this.fechaEstudio = fechaEstudio;
     }
-    
+
+    public String getAnalisisPuntual() {
+        return analisisPuntual;
+    }
+
+    public void setAnalisisPuntual(String analisisPuntual) {
+        this.analisisPuntual = analisisPuntual;
+    }
+
+    public List<List<String>> getDescripcionTramos() {
+        return descripcionTramos;
+    }
+
+    public void setDescripcionTramos(List<List<String>> descripcionTramos) {
+        this.descripcionTramos = descripcionTramos;
+    }
+
+    public List<String> getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public void setTipoVehiculo(List<String> tipoVehiculo) {
+        this.tipoVehiculo = tipoVehiculo;
+    }
+
+    public List<List<String>> getHorarioDePeriodos() {
+        return horarioDePeriodos;
+    }
+
+    public void setHorarioDePeriodos(List<List<String>> horarioDePeriodos) {
+        this.horarioDePeriodos = horarioDePeriodos;
+    }
+
+    public List<List<String>> getNumEquipos() {
+        return numEquipos;
+    }
+
+    public void setNumEquipos(List<List<String>> numEquipos) {
+        this.numEquipos = numEquipos;
+    }
+
+    public double getVelTP_AM_NS_WE() {
+        return velTP_AM_NS_WE;
+    }
+
+    public void setVelTP_AM_NS_WE(double velTP_AM_NS_WE) {
+        this.velTP_AM_NS_WE = velTP_AM_NS_WE;
+    }
+
+    public double getVelTP_M_NS_WE() {
+        return velTP_M_NS_WE;
+    }
+
+    public void setVelTP_M_NS_WE(double velTP_M_NS_WE) {
+        this.velTP_M_NS_WE = velTP_M_NS_WE;
+    }
+
+    public double getVelTP_PM_NS_WE() {
+        return velTP_PM_NS_WE;
+    }
+
+    public void setVelTP_PM_NS_WE(double velTP_PM_NS_WE) {
+        this.velTP_PM_NS_WE = velTP_PM_NS_WE;
+    }
+
+    public double getVelTPC_AM_NS_WE() {
+        return velTPC_AM_NS_WE;
+    }
+
+    public void setVelTPC_AM_NS_WE(double velTPC_AM_NS_WE) {
+        this.velTPC_AM_NS_WE = velTPC_AM_NS_WE;
+    }
+
+    public double getVelTPC_M_NS_WE() {
+        return velTPC_M_NS_WE;
+    }
+
+    public void setVelTPC_M_NS_WE(double velTPC_M_NS_WE) {
+        this.velTPC_M_NS_WE = velTPC_M_NS_WE;
+    }
+
+    public double getVelTPC_PM_NS_WE() {
+        return velTPC_PM_NS_WE;
+    }
+
+    public void setVelTPC_PM_NS_WE(double velTPC_PM_NS_WE) {
+        this.velTPC_PM_NS_WE = velTPC_PM_NS_WE;
+    }
+
+    public double getVelTPI_AM_NS_WE() {
+        return velTPI_AM_NS_WE;
+    }
+
+    public void setVelTPI_AM_NS_WE(double velTPI_AM_NS_WE) {
+        this.velTPI_AM_NS_WE = velTPI_AM_NS_WE;
+    }
+
+    public double getVelTPI_M_NS_WE() {
+        return velTPI_M_NS_WE;
+    }
+
+    public void setVelTPI_M_NS_WE(double velTPI_M_NS_WE) {
+        this.velTPI_M_NS_WE = velTPI_M_NS_WE;
+    }
+
+    public double getVelTPI_PM_NS_WE() {
+        return velTPI_PM_NS_WE;
+    }
+
+    public void setVelTPI_PM_NS_WE(double velTPI_PM_NS_WE) {
+        this.velTPI_PM_NS_WE = velTPI_PM_NS_WE;
+    }
+
+    public double getVelTP_AM_SN_EW() {
+        return velTP_AM_SN_EW;
+    }
+
+    public void setVelTP_AM_SN_EW(double velTP_AM_SN_EW) {
+        this.velTP_AM_SN_EW = velTP_AM_SN_EW;
+    }
+
+    public double getVelTP_M_SN_EW() {
+        return velTP_M_SN_EW;
+    }
+
+    public void setVelTP_M_SN_EW(double velTP_M_SN_EW) {
+        this.velTP_M_SN_EW = velTP_M_SN_EW;
+    }
+
+    public double getVelTP_PM_SN_EW() {
+        return velTP_PM_SN_EW;
+    }
+
+    public void setVelTP_PM_SN_EW(double velTP_PM_SN_EW) {
+        this.velTP_PM_SN_EW = velTP_PM_SN_EW;
+    }
+
+    public double getVelTPC_AM_SN_EW() {
+        return velTPC_AM_SN_EW;
+    }
+
+    public void setVelTPC_AM_SN_EW(double velTPC_AM_SN_EW) {
+        this.velTPC_AM_SN_EW = velTPC_AM_SN_EW;
+    }
+
+    public double getVelTPC_M_SN_EW() {
+        return velTPC_M_SN_EW;
+    }
+
+    public void setVelTPC_M_SN_EW(double velTPC_M_SN_EW) {
+        this.velTPC_M_SN_EW = velTPC_M_SN_EW;
+    }
+
+    public double getVelTPC_PM_SN_EW() {
+        return velTPC_PM_SN_EW;
+    }
+
+    public void setVelTPC_PM_SN_EW(double velTPC_PM_SN_EW) {
+        this.velTPC_PM_SN_EW = velTPC_PM_SN_EW;
+    }
+
+    public double getVelTPI_AM_SN_EW() {
+        return velTPI_AM_SN_EW;
+    }
+
+    public void setVelTPI_AM_SN_EW(double velTPI_AM_SN_EW) {
+        this.velTPI_AM_SN_EW = velTPI_AM_SN_EW;
+    }
+
+    public double getVelTPI_M_SN_EW() {
+        return velTPI_M_SN_EW;
+    }
+
+    public void setVelTPI_M_SN_EW(double velTPI_M_SN_EW) {
+        this.velTPI_M_SN_EW = velTPI_M_SN_EW;
+    }
+
+    public double getVelTPI_PM_SN_EW() {
+        return velTPI_PM_SN_EW;
+    }
+
+    public void setVelTPI_PM_SN_EW(double velTPI_PM_SN_EW) {
+        this.velTPI_PM_SN_EW = velTPI_PM_SN_EW;
+    }
+
+    public List<List<String>> getVelocidadesPromedio() {
+        return velocidadesPromedio;
+    }
+
+    public void setVelocidadesPromedio(List<List<String>> velocidadesPromedio) {
+        this.velocidadesPromedio = velocidadesPromedio;
+    }
+
 }
